18f63a6558acf06d5024d3852c7c9f73fc13b560
marc <m.de.groote@onsneteindhoven.nl>
2021-03-11 15:48:12 +0100

    Eerste versie prototype

--- OpenIDConnectAuthentication/AuthenticationController.cs
+++ OpenIDConnectAuthentication/AuthenticationController.cs
@@ -10,6 +10,7 @@ using System.Text;
 using System.IdentityModel.Tokens.Jwt;
 using Microsoft.IdentityModel.Tokens;
 using System.Net.Http;
+using Microsoft.Extensions.Configuration;
 
 namespace OpenIDConnectAuthentication
 {
@@ -17,41 +18,53 @@ namespace OpenIDConnectAuthentication
     [ApiController]
     public class AuthenticationController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
+
+        public AuthenticationController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         [HttpGet]
         [Route("login")]
-        public IActionResult Login([FromQuery]string returnurl)
+        public IActionResult Login([FromQuery]Uri returnurl)
         {
             if (!HttpContext.User.Identity.IsAuthenticated)
             {
                 return Challenge("Microsoft");
             }
 
-            string token = CreateJwtToken();
+            string token = CreateJwtToken(returnurl.Authority);
             Response.Cookies.Append("jwttoken", token);
 
-            return Redirect(returnurl);
+            return Redirect(returnurl.AbsoluteUri);
         }
 
         [HttpGet]
         [Route("logout")]
-        [Authorize]
-        public async Task Logout()
+        public IActionResult Logout([FromQuery]Uri returnurl)
         {
-            await Microsoft.AspNetCore.Authentication.AuthenticationHttpContextExtensions.SignOutAsync(HttpContext, Microsoft.AspNetCore.Authentication.Cookies.CookieAuthenticationDefaults.AuthenticationScheme);
+            if (!HttpContext.User.Identity.IsAuthenticated)
+                return Redirect(returnurl.AbsoluteUri);
+
+            Microsoft.AspNetCore.Authentication.AuthenticationHttpContextExtensions.SignOutAsync(HttpContext, Microsoft.AspNetCore.Authentication.Cookies.CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return Redirect(returnurl.AbsoluteUri);
         }
 
-        private string CreateJwtToken()
+        private string CreateJwtToken(string audience)
         {
             var tokenHandler = new JwtSecurityTokenHandler();
 
-            var key = Encoding.ASCII.GetBytes("SuperDuberUltraSecretKeyOfPureAwesomness");
+            var key = Encoding.ASCII.GetBytes(_configuration["signingkey"]);
 
             var tokenDescriptor = new SecurityTokenDescriptor
             {
                 Issuer = "localhost",
+                Audience = audience,
                 Expires = DateTime.UtcNow.AddMinutes(15),
                 Subject = new System.Security.Claims.ClaimsIdentity(new List<System.Security.Claims.Claim> { HttpContext.User.Claims.First(x => x.Type == "preferred_username") }),
-                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.RsaSha256)
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256)
             };
 
             var token = tokenHandler.CreateToken(tokenDescriptor);
---
+++ OpenIDConnectAuthentication/JWTController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenIDConnectAuthentication
+{
+    [Route("jwt")]
+    [ApiController]
+    public class JWTController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public JWTController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public IActionResult VerifyToken([FromForm]string token)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var key = Encoding.ASCII.GetBytes(_configuration["signingkey"]);
+
+            try
+            {
+                var claims = tokenHandler.ValidateToken(token, new TokenValidationParameters
+                {
+                    ValidAlgorithms = new List<string>() { "HS256"},
+                    ValidateAudience = false,
+                    ValidateIssuer = true,
+                    ValidIssuer = "localhost",
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ClockSkew = TimeSpan.Zero,
+                    IssuerSigningKey = new SymmetricSecurityKey(key)
+                }, out SecurityToken securityToken);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500);
+            }
+
+            return Ok();
+            
+        }
+    }
+}
